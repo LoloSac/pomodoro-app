72661006808bd208e686e7d42e94bdfb5f44d96e
LoloSac <lorenzosacerdote@gmail.com>
2022-08-08 23:55:12 -0300

    cambio timer no puede bajar de 0, comentarios

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,9 +3,9 @@ const Timer = () => {
 
 const timeBuilder = (initialTime, initialDate, dateNow) => {
   let seconds = (initialTime-(dateNow-initialDate))/1000;
-  return(`${Math.floor(seconds/60)}:${new Intl.NumberFormat('nu', {minimumIntegerDigits:'2'}).format((Math.floor(seconds%60)))}`)
+  return(seconds >= 0 ? `${Math.floor(seconds/60)}:${new Intl.NumberFormat('nu', {minimumIntegerDigits:'2'}).format((Math.floor(seconds%60)))}` : '0:00')
 }
-const [initialTime, setInitialTime] = useState(63*1000);
+const [initialTime, setInitialTime] = useState(5*1000);
 const [initialDate, setInitialDate] = useState(0);
 const [dateNow, setDateNow] = useState(0);
   
@@ -29,3 +29,7 @@ return (
   </span>
 )}
 export default Timer;
+
+// todo: necesito hacer que el setInterval pare cuando el número llegue a 0, por lo que capaz me conviene
+// hacer otro componente que se llame algo como timerTime o algo así y si seconds ≤ 0 que no se renderice más, 
+// parando el setInterval por la cleanup function del useEffect
